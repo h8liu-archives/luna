f87811b8dbf3a3098b58b8d7656a4faca516f954
He Liu <liulonnie@gmail.com>
2014-09-26 11:04:51 -0700

    cpu simulation

--- sim/cpu.go
+++ sim/cpu.go
@@ -1,31 +1,5 @@
 package sim
 
-const Nreg = 16
-
-type regs struct {
-	usr []uint32
-	svc []uint32
-	mon []uint32
-	abt []uint32
-	und []uint32
-	irq []uint32
-	fiq []uint32
-}
-
-func newRegs() *regs {
-	ret := new(regs)
-
-	ret.usr = make([]uint32, Nreg)
-	ret.svc = make([]uint32, Nreg)
-	ret.mon = make([]uint32, Nreg)
-	ret.abt = make([]uint32, Nreg)
-	ret.und = make([]uint32, Nreg)
-	ret.irq = make([]uint32, Nreg)
-	ret.fiq = make([]uint32, Nreg)
-
-	return ret
-}
-
 // Currnet program status register
 type cpsr struct {
 	n, z, c, v, q, e bool
@@ -46,17 +20,29 @@ type CPU struct {
 	regs *regs
 	cpsr *cpsr
 	spsr *spsr
+
+	phyMem *PhyMemory
 }
 
-func NewCPU() *CPU {
+func NewCPU(mem *PhyMemory) *CPU {
 	ret := new(CPU)
 	ret.regs = newRegs()
 	ret.cpsr = new(cpsr)
 	ret.spsr = new(spsr)
+
+	ret.phyMem = mem
 
 	return ret
 }
 
 func (cpu *CPU) isPriviledged() bool {
 	panic("todo")
+}
+
+func (cpu *CPU) Reset(pc uint32) {
+	// TODO:
+}
+
+func (cpu *CPU) Step() {
+	// TODO:
 }
