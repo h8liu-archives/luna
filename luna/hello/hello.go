10094d1db60764892e820406cfda4a3ad2f28fc4
He Liu <liulonnie@gmail.com>
2014-09-26 23:32:49 -0700

    some comments on the uart stuff

--- luna/hello/hello.go
+++ luna/hello/hello.go
@@ -23,7 +23,9 @@ func Img() (code, data []byte) {
 		Ld(R3, R1, 4), // uart0 base
 
 		Movi(R0, 0),
-		St(R0, R3, 0x30), // disable uart0
+		// CR = 0, diable UART
+		St(R0, R3, 0x30), // CR, disable uart0
+
 		St(R0, R2, 0x94), // disable pull up/down
 		// delay150
 		Movi(R0, 150),
@@ -43,28 +45,57 @@ func Img() (code, data []byte) {
 		Movi(R0, 0),
 		St(R0, R2, 0x98), // GPPUDCLK0 make it take effect
 
+		// UART ICR = 0x7ff;
+		// Clearing pending interrupts
+		// TODO: according to the document, it should write 0x7f2
+		// since the 3 bits are not supported and should write zero
+		//
+		// also, according to the doc, after disable the UART, one should
+		// - wait for the end of tx or rx of the current character (how? just spin?)
+		// - flush the transmit FIFO by setting the FEN bit to 0 in the LCRH
+		// and then one can reprogram stuff, and enable UART.
 		Movi(R0, 0x7),
 		Sllv(R0, R0, 8),
 		Ori(R0, R0, 0xff), // 0x7ff
 		St(R0, R3, 0x44),
 
+		// IBRD = 1, FBRD = 40
+		// TODO: what is the baud rate?
 		Movi(R0, 1),
 		St(R0, R3, 0x24), // IBRD
 		Movi(R0, 40),
 		St(R0, R3, 0x28), // FBRD
 
+		// LDRH = 0x70
+		// word length = 8 bits
+		// FIFO enabled
+		// stick parity disabled
+		// no two stop bits
+		// no parity
+		// no send break
 		Movi(R0, 0x70),
 		St(R0, R3, 0x2C), // LCRH
 
+		// UART IMSC = 0x7f2
+		// interrupt mask
+		// this will enable all the supported interrupts
 		Movi(R0, 0x7),
 		Sllv(R0, R0, 8),
 		Ori(R0, R0, 0xf2),
 		St(R0, R3, 0x38), // IMSC
 
+		// CR = 0x301
+		// no CTS control enable
+		// no RTS control enable
+		// set RTS to 0 (since it is not connected, so we don't care)
+		// enable rx (bit 9)
+		// enable tx (bit 8)
+		// loopback disabled (bit 7)
+		// uart enabled (bit 0)
 		Movi(R0, 0x3),
 		Sllv(R0, R0, 8),
 		Ori(R0, R0, 0x1),
-		St(R0, R3, 0x30), // enable uart
+		St(R0, R3, 0x30), // CR, enable uart
 
 		Addi(R0, R1, 8), // start of the string
 
@@ -75,12 +106,15 @@ func Img() (code, data []byte) {
 
 		// byte is now saved in R4
 		// wait_loop:
+		// read in FR, flag register
 		Ld(R5, R3, 0x18),
+		// check bit 5, see if tx fifo is full
 		Srlv(R5, R5, 5),
 		Andi(R5, R5, 0x1),
 		Cmpi(R5, 0),
 		Bne(-6), // break on zero, continue pulling on 1
 
+		// write DR, the data register
 		St(R4, R3, 0), // write the byte out
 		Addi(R0, R0, 1),
 		J(-12), // goto loop
